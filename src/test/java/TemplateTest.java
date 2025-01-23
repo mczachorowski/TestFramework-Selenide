27d0f3975d14e5a623934831b8db829f5b77173a
mariusz <hagnar93@gmail.com>
2025-01-23 16:19:56 +0100

    Selenide + Testng dependencies added First working test added

--- src/test/java/TemplateTest.java
+++ src/test/java/TemplateTest.java
@@ -1,2 +1,24 @@
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class TemplateTest {
+    private final SelenideElement searchArea = $(By.xpath("//textarea[@aria-label='Szukaj']"));
+    private final SelenideElement rejectCookies = $(By.xpath("//div[@class='QS5gu sy4vM' and text() = 'Odrzuć wszystko']"));
+    private final SelenideElement acceptCookies = $(By.xpath("//div[@class='QS5gu sy4vM' and text() = 'Zaakceptuj wszystko']"));
+
+    @Test
+    public void userOpensWebsite() {
+        open("https://google.pl");
+        rejectCookies.shouldBe(visible, Duration.ofSeconds(5));
+        acceptCookies.shouldBe(visible, Duration.ofSeconds(5)).click();
+        searchArea.shouldBe(visible, Duration.ofSeconds(5));
+    }
+
 }
