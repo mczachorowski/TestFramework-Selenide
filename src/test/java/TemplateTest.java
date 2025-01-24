31b20ad1bceb0fb621af9a028a941e8e8190959d
mariusz <hagnar93@gmail.com>
2025-01-24 19:57:15 +0100

    Added one css selector for testing purposes

--- src/test/java/TemplateTest.java
+++ src/test/java/TemplateTest.java
@@ -9,7 +9,8 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TemplateTest {
-    private final SelenideElement searchArea = $(By.xpath("//textarea[@aria-label='Szukaj']"));
+    private final SelenideElement searchAreaXpath = $(By.xpath("//textarea[@aria-label='Szukaj']"));
+    private final SelenideElement searchAreaCss = $(By.cssSelector("[title='Szukaj']"));
     private final SelenideElement rejectCookies = $(By.xpath("//div[@class='QS5gu sy4vM' and text() = 'Odrzuć wszystko']"));
     private final SelenideElement acceptCookies = $(By.xpath("//div[@class='QS5gu sy4vM' and text() = 'Zaakceptuj wszystko']"));
 
@@ -18,7 +19,8 @@ public class TemplateTest {
         open("https://google.pl");
         rejectCookies.shouldBe(visible, Duration.ofSeconds(5));
         acceptCookies.shouldBe(visible, Duration.ofSeconds(5)).click();
-        searchArea.shouldBe(visible, Duration.ofSeconds(5));
+        searchAreaCss.shouldBe(visible, Duration.ofSeconds(5));
+        searchAreaXpath.shouldBe(visible, Duration.ofSeconds(5));
     }
 
 }
